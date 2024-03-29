66ebe1b50f08217e8e865c766482613185a5279b
Esther-Naserian <naseriansamuria@gmail.com>
2024-03-29 11:54:39 +0300

    add files

---
+++ function.js
@@ -0,0 +1,15 @@
+let arr = ["Bus","Car","nissan",]
+function arrSort(arr){
+    arr.sort();
+    arr.reverse();
+    return arr;
+}
+console.log(arrsort(arr));
+
+//questions
+function changeArray(){
+    const firstFour = arr.slice(0,4).map(item=>items*8)
+    const lastTwo= arr.slice(-2),map(items=>items+5);
+    const newArr = firstFour.concat(lastTwo);
+    console.log(newArr);
+}
